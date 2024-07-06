2044f5d31c0898875807726a1945c21dfcb545f4
Czékus Máté <mate@picloud.hu>
2024-07-06 20:28:40 +0200

    feat: add new treesitter language supports + telescope diagnose keybind
    Signed-off-by: Czékus Máté <mate@picloud.hu>

--- lua/autocommands.lua
+++ lua/autocommands.lua
@@ -1,22 +1,25 @@
 vim.api.nvim_create_autocmd('LspAttach', {
   desc = 'LSP actions',
   callback = function(event)
+    local telescope = require('telescope.builtin')
     -- these will be buffer-local keybindings
     -- because they only work if you have an active language server
     vim.keymap.set('n', 'K', vim.lsp.buf.hover, { buffer = event.buf })
-    vim.keymap.set('n', '<leader>gd', require('telescope.builtin').lsp_definitions,
+    vim.keymap.set('n', '<leader>gd', telescope.lsp_definitions,
       { buffer = event.buf, desc = '[g]oto [d]efinitions' })
     vim.keymap.set('n', '<leader>gi', '<cmd>lua vim.lsp.buf.implementation()<cr>',
       { buffer = event.buf, desc = '[g]oto [i]mplementations' })
-    vim.keymap.set('n', '<leader>gr', require('telescope.builtin').lsp_references,
+    vim.keymap.set('n', '<leader>gr', telescope.lsp_references,
       { buffer = event.buf, desc = '[g]oto [r]eferences' })
     vim.keymap.set('n', '<leader>sh', vim.lsp.buf.signature_help, { buffer = event.buf, desc = '[s]ignature [h]elp' })
     vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, { buffer = event.buf, desc = '[r]e[n]ame' })
     vim.keymap.set({ 'n', 'x' }, '<C-A-l>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>',
       { buffer = event.buf, desc = 'Format [C-A-l]' })
     vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, { buffer = event.buf, desc = '[c]ode [a]actions' })
-    vim.keymap.set('n', '<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols,
+    vim.keymap.set('n', '<leader>ws', telescope.lsp_dynamic_workspace_symbols,
       { buffer = event.buf, desc = '[w]orkspace [s]ymbols' })
+    vim.keymap.set('n', '<leader>wd', telescope.diagnostics,
+      { buffer = event.buf, desc = '[w]orkspace [d]iagnostics' })
   end
 })
 
